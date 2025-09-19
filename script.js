8e092327dc573806b7f944f7184ebf40f230efe8
Mahery ANDRIANANDRAINA <maheryelo2@gmail.com>
2025-09-19 16:57:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// ====== Développement des project cards ======
+// Développement des project cards
 const projectCards = document.querySelectorAll('.project-card');
 
 projectCards.forEach(card => {
   card.addEventListener('click', () => {
+    // bascule la classe "active" sur la card cliquée
     card.classList.toggle('active');
   });
 });
