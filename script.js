607b62c0dcf22a4bf8f733be691ddd539fbda87b
Mahery ANDRIANANDRAINA <maheryelo2@gmail.com>
2025-09-29 17:18:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,8 @@ projectCards.forEach(card => {
     card.classList.toggle('active');
   });
 });
-function confirmDownload() {
-    let result = confirm("Do you want to download it?");
-    if (result) {
-      // Redirection vers le fichier si OUI
-      window.location.href = "file:///O:/CHECKERS/SAMPLES/MoM Checkers SS 2026 Dvpt.xlsx";
-    }
-    // Si NON, rien ne se passe
-  }
+
+
 
 
 
